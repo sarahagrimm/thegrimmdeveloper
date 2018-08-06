054c832fd11a595aff3a5483d7928e0a89b1aa73
Sarah Grimm <sarahagrimm@gmail.com>
2018-08-06 06:41:42 -0700

    made progress on my new portfolio using angular2 and bootstrap4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ServicesComponent } from './services/services.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    ContactMeComponent,
+    ServicesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
