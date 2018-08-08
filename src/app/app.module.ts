bb15227b318efc57e448dc439a93d14be62b17a9
Sarah Grimm <sarahagrimm@gmail.com>
2018-08-08 11:54:34 -0700

    added more details to portfolio website and optimized for mobile using bootstrap and media queries

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -8,6 +9,18 @@ import { SkillsComponent } from './skills/skills.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
 import { ServicesComponent } from './services/services.component';
 import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+const appRoutes: Routes = [
+  
+  { path: 'home', component: HeaderComponent },
+  { path: 'about', component: AboutMeComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactMeComponent },
+  { path: 'portfolio', component: PortfolioComponent }
+
+];
+
 
 @NgModule({
   declarations: [
@@ -17,10 +30,15 @@ import { FooterComponent } from './footer/footer.component';
     SkillsComponent,
     ContactMeComponent,
     ServicesComponent,
-    FooterComponent
+    FooterComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true} )
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
